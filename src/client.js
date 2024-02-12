a6bce50d38e2503c79704e8aa3d26c8147c51402
Jude Chuks <judechuks26@gmail.com>
2024-02-12 17:27:59 +0100

    had to use the root directory bcos of netlify

---
+++ src/client.js
@@ -0,0 +1,18 @@
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = createClient({
+  // projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: "2a0s3s0w",
+  dataset: "production",
+  apiVersion: "2024-02-02",
+  useCdn: true,
+  token:
+    "skRfgxa6CPZnFYRu9QGkilN26gAdWjgfUbuHq3PE1u3wQoUH4f1up2cJaPFl1fkNISiA4TGXbtvOIPld8aQqoqWxDYdwb223pb0v00BPte01uEySDnCujKeqgPqs6iK431Bj2scpnhsGAjUPn7U0I4Qwe4zn0V4pZaKtn8vAPab0FayvtLRi",
+  // token: process.env.REACT_APP_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
---
+++ src/components/EducationCard.jsx
@@ -0,0 +1,47 @@
+import { companyAlt } from "../assets/icons";
+import { urlFor } from "../client";
+
+const EducationCard = ({
+  id,
+  imageurl = companyAlt,
+  certification,
+  institution,
+  location,
+  period,
+  skills,
+  bgcolor,
+}) => {
+  return (
+    <article className="group min-[850px]:w-[50%] dark:before:border-white relative">
+      <div className="card py-6 px-3 rounded-lg border-2 border-text-gray relative overflow-hidden transition duration-500 cursor-pointer">
+        <header className="mb-4">
+          <img src={urlFor(imageurl)} alt={institution} className="w-20 h-12" />
+        </header>
+        <section className="mb-4">
+          <h4 className="font-bold text-slate-gray">{certification}</h4>
+          <h5 className="text-slate-gray leading-6 my-2">{institution}</h5>
+          <h6 className="font-semibold text-slate-gray">
+            {location} - {period}
+          </h6>
+        </section>
+        <footer className="flex flex-wrap gap-1">
+          {skills.map((skill, index) => (
+            <span
+              key={index}
+              className="min-w-12 text-center px-2 py-1 border border-slate-gray rounded-lg hover:bg-slate-gray">
+              {skill}
+            </span>
+          ))}
+        </footer>
+        {/* Overlay background effect */}
+        <div
+          className={`overlay-bg absolute top-0 left-0 bottom-0 right-0 rounded-full scale-0 duration-500 ${bgcolor} group-hover:opacity-0`}></div>
+      </div>
+      {/* circle */}
+      <div className={`circle size-8 absolute rounded-full bg-blue-400`}>
+        <span className="font-semibold bg-white dark:bg-body-clr">{id}</span>
+      </div>
+    </article>
+  );
+};
+export default EducationCard;
