bfd43ee5de6efa549a747dc343a376d9ad7e80c6
Jude Chuks <judechuks26@gmail.com>
2024-02-12 16:57:00 +0100

    commented .env config, using default vite config

--- frontend_react/vite.config.js
+++ frontend_react/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
+/*
 //vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -9,10 +10,9 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
   };
 });
+*/
 
-/*
 // Default setting
 export default defineConfig({
   plugins: [react()],
 });
-*/
