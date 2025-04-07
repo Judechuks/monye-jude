e90c08dcbfecebb88973b4f045bfda6bea557348
Jude Chuks <judechuks26@gmail.com>
2025-04-07 22:57:05 +0100

    used react-tilt package to tilt award and project cards

--- src/sections/Award.jsx
+++ src/sections/Award.jsx
@@ -6,6 +6,19 @@ import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { Tilt } from "react-tilt";
+
+const defaultOptions = {
+  reverse: false, // reverse the tilt direction
+  max: 15, // max tilt rotation (degrees)
+  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1.0, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 1000, // Speed of the enter/exit transition
+  transition: true, // Set a transition on enter/exit.
+  axis: null, // What axis should be disabled. Can be X or Y.
+  reset: true, // If the tilt effect has to be reset on exit.
+  easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+};
 
 const Award = () => {
   const [awards, setAwards] = useState([]);
@@ -28,10 +41,13 @@ const Award = () => {
         // spaceBetween={50}
         slidesPerView={1}
         navigation
-        pagination={{ clickable: true }}>
+        pagination={{ clickable: true }}
+      >
         {awards.map((award, index) => (
           <SwiperSlide key={index}>
-            <AwardCard key={index} {...award} />
+            <Tilt options={defaultOptions}>
+              <AwardCard key={index} {...award} />
+            </Tilt>
           </SwiperSlide>
         ))}
       </Swiper>
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,6 +4,19 @@ import { ProjectCard } from "../components";
 import { useGlobalContext } from "../context/globalContext";
 import { AppWrap } from "../wrapper";
 // import { projects } from "../constants";
+import { Tilt } from "react-tilt";
+
+const defaultOptions = {
+  reverse: false, // reverse the tilt direction
+  max: 35, // max tilt rotation (degrees)
+  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1.03, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 1000, // Speed of the enter/exit transition
+  transition: true, // Set a transition on enter/exit.
+  axis: null, // What axis should be disabled. Can be X or Y.
+  reset: true, // If the tilt effect has to be reset on exit.
+  easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+};
 
 const Projects = () => {
   let skills = [];
@@ -50,7 +63,9 @@ const Projects = () => {
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
         {portfolioProjects.map((project, index) => (
           <Link to={`/project/${project.id}`} key={index}>
-            <ProjectCard key={project.id} {...project} />
+            <Tilt options={defaultOptions}>
+              <ProjectCard key={project.id} {...project} />
+            </Tilt>
           </Link>
         ))}
       </div>
