209837270e9ff3592ece6b29f867db7206230dfd
Jude Chuks <judechuks26@gmail.com>
2025-05-22 18:37:52 +0100

    filter buttons

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -8,10 +8,10 @@ import { Tilt } from "react-tilt";
 
 const defaultOptions = {
   reverse: false, // reverse the tilt direction
-  max: 35, // max tilt rotation (degrees)
-  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+  max: 15, // max tilt rotation (degrees)
+  perspective: 5000, // Transform perspective, the lower the more extreme the tilt gets.
   scale: 1.03, // 2 = 200%, 1.5 = 150%, etc..
-  speed: 1000, // Speed of the enter/exit transition
+  speed: 500, // Speed of the enter/exit transition
   transition: true, // Set a transition on enter/exit.
   axis: null, // What axis should be disabled. Can be X or Y.
   reset: true, // If the tilt effect has to be reset on exit.
@@ -45,18 +45,19 @@ const Projects = () => {
       <div className="section-underline"></div>
       <article className="flex gap-3 mb-12 flex-wrap">
         {skills.map((item, index) => (
-          <p
+          <button
+            type="button"
             key={index}
             onClick={() => {
               filterSkills(item);
               setActive(item);
             }}
             className={`${
-              item == active && "bg-blue-800 text-white "
-            } p-1 border rounded-md uppercase text-sm cursor-pointer hover:bg-blue-800 hover:text-white duration-500`}
+              item == active && "bg-blue-800 text-white"
+            } py-2 px-3 mt-4 border rounded-md uppercase text-sm cursor-pointer focus:ring-offset-1 focus:ring-2 focus:ring-blue-700 hover:bg-blue-800 hover:text-white duration-500`}
           >
             {item}
-          </p>
+          </button>
         ))}
       </article>
 
