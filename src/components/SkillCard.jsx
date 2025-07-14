c1b309d6470cc297161f68dd1282a30d8d41661f
Jude Chuks <judechuks26@gmail.com>
2025-07-14 15:27:09 +0100

    updated bg color

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -4,9 +4,9 @@ import { urlFor } from "../client";
 const SkillCard = ({ skill, imageurl, bgcolor }) => {
   return (
     <div
-      className={`bg-[#13aa52] skills group relative z-30 cursor-pointer h-12 overflow-hidden transition-all duration-700`}
+      className={`${bgcolor} skills group relative z-30 cursor-pointer h-12 overflow-hidden transition-all duration-700`}
     >
-      <p className="p-2 h-full bg-white-400 dark-bg-body-clr dark:text-white w-full absolute z-0">
+      <p className="p-2 h-full bgwhite-400 darkbg-body-clr dark:text-white w-full absolute z-0">
         {skill}
       </p>
       <img
@@ -17,7 +17,7 @@ const SkillCard = ({ skill, imageurl, bgcolor }) => {
 
       {/* Overlay background effect */}
       <div
-        className={`bg-[#13aa52] z-10 -top-1 rounded-full group-hover:w-full group-hover:opacity-30 absolute w-0 h-12 transition-all duration-500 opacity-100`}
+        className={`${bgcolor} z-10 -top-1 rounded-full group-hover:w-full group-hover:opacity-30 absolute w-0 h-12 transition-all duration-500 opacity-100`}
       ></div>
     </div>
   );
